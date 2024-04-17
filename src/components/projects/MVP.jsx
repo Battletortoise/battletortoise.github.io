4c66b50b18326a80fb74e76f7c36893b26e4e4ce
Wyatt Grayson Fulbright <wgfulbright@gmail.com>
2024-04-17 16:29:43 -0700

    added new gif from GGD

--- src/components/projects/MVP.jsx
+++ src/components/projects/MVP.jsx
@@ -4,17 +4,26 @@ const MVP = () => {
   return (
     <div class="container">
       <div class="card">
-        <div class="card-content">
-          <div class="content">
-            <h1>Granblue Grid Designer</h1>
-            <p>The GGD is a personal project I made for the mobile game 'Granblue Fantasy'. It simulates the grid building aspect of gbf, calculating final grid stats through a series of algorythms</p>
-            <h3>Stack</h3>
-            <ul>
-              <li>Javascript</li>
-              <li>React</li>
-              <li>Bulma CSS</li>
-              <li>Webpack</li>
-            </ul>
+        <div class="is-flex">
+          <div class="card-content">
+            <div class="content">
+              <h1>Granblue Grid Designer</h1>
+              <p>The GGD is a personal project I made for the mobile game 'Granblue Fantasy'. It simulates the grid building aspect of gbf, calculating final grid stats through a series of algorythms</p>
+              <h3>Stack</h3>
+              <ul>
+                <li>Javascript</li>
+                <li>React</li>
+                <li>Bulma CSS</li>
+                <li>Webpack</li>
+              </ul>
+            </div>
+          </div>
+          <div class="card-content">
+            <div class="card-image">
+              <figure class="image is 128x128">
+                <img src="/assets/GGD.gif" alt="Placeholder image" />
+              </figure>
+            </div>
           </div>
         </div>
         <footer class="card-footer">
