3b967db6aeae90bb31765c9a89efae2094c8d503
Wyatt Grayson Fulbright <wgfulbright@gmail.com>
2024-05-01 14:33:04 -0400

    pre changing stack to icons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Projects from './Projects.jsx';
 import About from './About.jsx';
+import Characters from './Characters.jsx';
 
 const Home = () => {
   return (
@@ -11,6 +12,9 @@ const Home = () => {
       <section class="section container">
         <Projects />
       </section>
+      {/* <section class="section container">
+        <Characters />
+      </section> */}
     </div>
   )
 };
---
+++ src/data/characters.js
@@ -0,0 +1,176 @@
+const characters = [
+  {
+    name: "Addison",
+    description: "",
+    moli: "",
+    ability: "",
+    avatar: "/characters/Addison.png"
+  },
+  {
+    name: "Aiden",
+    description: "",
+    moli: "",
+    ability: "",
+    avatar: "/characters/Aiden.png"
+  },
+  {
+    name: "Anna",
+    description: "",
+    moli: "",
+    ability: "",
+    avatar: "/characters/Anna.png"
+  },
+  {
+    name: "Arthur",
+    description: "",
+    moli: "",
+    ability: "",
+    avatar: "/characters/Arthur.png"
+  },
+  {
+    name: "Aveva",
+    description: "",
+    moli: "",
+    ability: "",
+    avatar: "/characters/Aveva.png"
+  },
+  {
+    name: "Bailey",
+    description: "",
+    moli: "",
+    ability: "",
+    avatar: "/characters/Bailey.png"
+  },
+  {
+    name: "Cinder",
+    description: "",
+    moli: "",
+    ability: "",
+    avatar: "/characters/Cinder.png"
+  },
+  {
+    name: "Evie",
+    description: "",
+    moli: "",
+    ability: "",
+    avatar: "/characters/Evie.png"
+  },
+  {
+    name: "Garron",
+    description: "",
+    moli: "",
+    ability: "",
+    avatar: "/characters/Garron.png"
+  },
+  {
+    name: "Grayson",
+    description: "",
+    moli: "",
+    ability: "",
+    avatar: "/characters/Grayson.png"
+  },
+  {
+    name: "Harrison",
+    description: "",
+    moli: "",
+    ability: "",
+    avatar: "/characters/Harrison.png"
+  },
+  {
+    name: "Harry",
+    description: "",
+    moli: "",
+    ability: "",
+    avatar: "/characters/Harry.png"
+  },
+  {
+    name: "Linus",
+    description: "",
+    moli: "",
+    ability: "",
+    avatar: "/characters/Linus.png"
+  },
+  {
+    name: "Matthew",
+    description: "",
+    moli: "",
+    ability: "",
+    avatar: "/characters/Matthew.png"
+  },
+  {
+    name: "Maverick",
+    description: "",
+    moli: "",
+    ability: "",
+    avatar: "/characters/Maverick.png"
+  },
+  {
+    name: "Michael",
+    description: "",
+    moli: "",
+    ability: "",
+    avatar: "/characters/Michael.png"
+  },
+  {
+    name: "Morgan",
+    description: "",
+    moli: "",
+    ability: "",
+    avatar: "/characters/Morgan.png"
+  },
+  {
+    name: "Oliver",
+    description: "",
+    moli: "",
+    ability: "",
+    avatar: "/characters/Oliver.png"
+  },
+  {
+    name: "Penny",
+    description: "",
+    moli: "",
+    ability: "",
+    avatar: "/characters/Penny.png"
+  },
+  {
+    name: "Percy",
+    description: "",
+    moli: "",
+    ability: "",
+    avatar: "/characters/Percy.png"
+  },
+  {
+    name: "Tommy",
+    description: "",
+    moli: "",
+    ability: "",
+    avatar: "/characters/Tommy.png"
+  },
+  {
+    name: "Tony",
+    description: "",
+    moli: "",
+    ability: "",
+    avatar: "/characters/Tony.png"
+  },
+  {
+    name: "Zander",
+    description: "",
+    moli: "",
+    ability: "",
+    avatar: "/characters/Zander.png"
+  }
+];
+
+
+// {
+//   name: "",
+//   description: "",
+//   moli: "",
+//   ability: "",
+//   avatar: ""
+// }
+
+//   /characters/CharacterName.png
+
+export default characters;
